45e2174e8e2d66f689d3d59d2a6847838ddf8541
cjundang <cjundang@gmail.com>
2021-01-11 15:53:00 +0700

    add icon

--- index.php
+++ index.php
@@ -8,12 +8,16 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-3.5.1.min.js"></script>
 
+    <link rel="stylesheet" href="icon/css/all.css">
+
     <link rel="stylesheet" href="css/mystyle.css" >
     <script src="js/default.js"></script>
 
 </head>
 <body>
     <div class="container">
+        <i class="fab fa-accessible-icon"></i>
+
         <button class="btn btn-primary"> xxxx </button>
         <button class="btn btn-primary"> xxxx </button>
 
